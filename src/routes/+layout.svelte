971fc11ce6efdb269857f3a759d9cca07aff266b
Amarneethi <amarneethi@gmail.com>
2025-11-16 15:53:30 +0530

    Removed google fonts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,16 +18,6 @@
 		content="Turn your designs into functional interfaces, demos and production grade code in minutes, not weeks."
 	/>
 	<link rel="icon" href={favicon} />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Ephesis&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-		rel="stylesheet"
-		media={mediaAll ? 'all' : 'print'}
-		onload={() => {
-			mediaAll = true;
-		}}
-	/>
 </svelte:head>
 
 {@render children()}
